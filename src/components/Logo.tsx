96f0c07ad957bcdc1a47b4c5add3f34b68745325
403hpns <dev.403hpns@pm.me>
2023-06-19 12:21:55 +0200

    .

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import { AiFillCode } from "react-icons/ai";
 const Logo = () => {
   return (
     <div className="text-7xl cursor-pointer hover:text-primary hover:scale-105">
-      <Link href="/#">
+      <Link href="#">
         <AiFillCode />
       </Link>
     </div>
