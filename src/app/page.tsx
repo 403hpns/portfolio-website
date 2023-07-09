74ae69d09bc35b0b8932baa4b97ba54f9c76c24f
403hpns <dev.403hpns@pm.me>
2023-07-09 18:07:32 +0200

    feat: add content into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import Navbar from "@/components/sections/Navbar";
+import Header from "@/components/sections/Header";
+import About from "@/components/sections/About";
+import MyProjects from "@/components/sections/MyProjects";
+import Contact from "@/components/sections/Contact";
+import Footer from "@/components/sections/Footer";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="container mx-auto px-8 py-5 flex flex-col gap-40 z-10 overflow-x-hidden">
+        <div>
+          <Navbar />
+          <Header />
+        </div>
+        <main className="flex flex-col gap-40">
+          <About />
+          <MyProjects />
+          <Contact />
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
